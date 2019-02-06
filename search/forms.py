d9d4d2f5a4876d7f3d199d438cff5d3a41390b5d
coderjaymoh <lelerukjaymoh@gmail.com>
2019-02-07 03:41:11 +0400

    Some progress with forms

--- search/forms.py
+++ search/forms.py
@@ -1,9 +1,8 @@
 from django import forms
-
 from .models import search
 
 class searchForm(forms.ModelForm):
 
     class Meta:
         model = search
-        fields = ('item')
+        fields = ('search_item',)
--- search/views.py
+++ search/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import search
 from django.utils import timezone
+from .forms import searchForm
 
 def index(request):
 	return render(request, 'index.html')
@@ -8,4 +9,7 @@ def index(request):
 def search_list(request):
     searchs = search.objects.all()
     return render(request, 'index.html', {'searchs': searchs})
-	
+
+def new_search(request):
+    form = searchForm()
+    return render(request, 'index.html', {'form': form})
